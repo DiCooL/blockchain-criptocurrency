970ea8792e0cf3458bf93705251d24bc24602669
DiCooL <leonidslavesnov145@gmail.com>
2025-11-01 22:59:42 +0300

    Add files via upload

--- vote.sol
+++ vote.sol
@@ -6,7 +6,6 @@ contract Election {
     address private owner;
 
     string[] public electors;
-    //["trampe","piton","frs","bisos"]
 
     uint256 private maxVotes;
 
@@ -18,6 +17,17 @@ contract Election {
 
     mapping(uint256 => uint256) public numberOfVotes;
 
+    event voted(uint256 _index, address _voter);
+    event voteStopped();
+    event voteResumed();
+
+    error OnlyOwnerAllowed();
+    error ElectorsDoesNotExist(uint256 _pickedElector, uint256 totalElectors);
+    error YouCantVoteTwice();
+    error OwnerCantVote();
+    error MaxVotesReach(uint256 _maxVotes);
+    error VotingIsOver();
+
     constructor(string[] memory _electors, uint256 _maxVotes, uint256 _electionTime) {
         maxVotes = _maxVotes;
         electionTime = _electionTime + block.timestamp;
@@ -25,26 +35,53 @@ contract Election {
         owner = msg.sender;
     }
 
+    function getLeader() public view returns(uint256) {
+        uint256 leaderIndex;
+
+        for(uint i = 0; i < electors.length; i++) {
+            if(numberOfVotes[i] > numberOfVotes[leaderIndex]){
+                leaderIndex = i;
+            }
+        }
+
+        return leaderIndex;
+    }
+
     function vote(uint256 _number) public {
-        require(userVotes[msg.sender]==false, "Your address can't vote");
-        require(_number < electors.length, "Elector does not exist");
-        require(votes < maxVotes, "Too much votes");
-        require(owner != msg.sender, "You can't vote");
-        require(block.timestamp <= electionTime, "Time the end");
+        require(userVotes[msg.sender]==false, YouCantVoteTwice());
+        require(_number < electors.length, ElectorsDoesNotExist(_number, electors.length));
+        require(votes < maxVotes, MaxVotesReach(maxVotes));
+        require(owner != msg.sender, OwnerCantVote());
+        require(block.timestamp <= electionTime, VotingIsOver());
 
         userVotes[msg.sender] = true;
         numberOfVotes[_number] += 1;
         votes += 1;
+
+        emit voted(_number, msg.sender);
     }
 
-    function stopVote() public {
-        require(msg.sender == owner, "Voting continue");
+    modifier onlyOwner() {
+        require(msg.sender == owner, OnlyOwnerAllowed());
+        _;
+    }
+
+    function stopVote() public onlyOwner {
         electionTime = block.timestamp;
+
+        emit voteStopped();
     }
 
-    function resetMaxVotes( uint256 _maxVotes) public {
-        require(msg.sender == owner, "You can't do it");
+    function resetMaxVotes(uint256 _maxVotes) public onlyOwner {
         maxVotes = _maxVotes;
+
+        emit voteResumed();
+    }
+
+    function resetEndTime(uint256 _newEndTime) public onlyOwner {
+        electionTime = _newEndTime;
+
+        emit voteResumed();
     }
 
 }
